820a295e5e294f241803d9b5a2d7701faa5f52d4
SeongbaePark1105 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-02-11 12:46:08 +0900

    Update sw1861.java
    수정 완료

--- day0210/src/SwExpertAcademy/sw1861.java
+++ day0210/src/SwExpertAcademy/sw1861.java
@@ -6,8 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class sw1861 {
-	static int TC, tc, N, arr[][], cnt, comresult[][], result[][];
-	static int q, w;
+	static int TC, tc, N, arr[][], cnt, resultCnt, resultRoom;
 	static int[] dy = { -1, 0, 1, 0 }; // 상, 우, 하, 좌
 	static int[] dx = { 0, 1, 0, -1 }; // 시계 방향으로 탐색
 
@@ -18,11 +17,8 @@ public class sw1861 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			arr = new int[N][N];
-			comresult = new int[N][N];
-			result = new int[N][N];
-			q = 0;
-			w = 0;
-			cnt = 1;
+			resultCnt = 0;
+			resultRoom = 0;
 			for (int y = 0; y < N; y++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int x = 0; x < N; x++) {
@@ -31,36 +27,28 @@ public class sw1861 {
 			}
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					square(i, j);
-					System.out.println(cnt);
-					comresult[i][j] = cnt;
-					cnt = 1;
-				}
-			}
+					cnt = square(i, j);
 
-			for (int k = 0; k < N; k++) {
-				for (int l = 0; l < N; l++) {
-					if (result[q][w] <= comresult[k][l]) {
-						if(arr[q][w] < arr[k][l])
-							continue;
-						q = k;
-						w = l;
+					if (resultCnt < cnt) {
+						resultCnt = cnt;
+						resultRoom = arr[i][j];
+					} else if (resultCnt == cnt) {
+						resultRoom = Math.min(resultRoom, arr[i][j]);
 					}
 				}
 			}
-			System.out.println(q+"::"+w);
-			System.out.println("#"+tc+" " + arr[q][w]);
+			System.out.println("#" + tc + " " + resultRoom + " " + resultCnt);
 		}
 	}
-
-	 static void square(int nowy, int nowx) {
+	// 
+	static int square(int nowy, int nowx) { // 선생님 강의를 보니 내가 처음에 짜려고 시도한 것은 DFS 방법
 		for (int d = 0; d < 4; d++) {
 			int nexty = nowy + dy[d];
-			int nextx = nowx = dx[d];
+			int nextx = nowx + dx[d];
 			if (nexty >= 0 && nexty < N && nextx >= 0 && nextx < N && arr[nexty][nextx] == arr[nowy][nowx] + 1) {
-				cnt++;
-				square(nexty, nextx);
+				return square(nexty, nextx) + 1;
 			}
 		}
+		return 1;
 	}
 }
