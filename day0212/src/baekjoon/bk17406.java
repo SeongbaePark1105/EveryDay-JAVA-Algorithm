68a21cfa1c624bb25bad0c3c30f43f3fe8a27a9c
SeongbaePark1105 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-02-13 17:04:28 +0900

    Update bk17406.java

--- day0212/src/baekjoon/bk17406.java
+++ day0212/src/baekjoon/bk17406.java
@@ -3,10 +3,14 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bk17406 {
-	static int N, M, K, A[][]; 
+	static int N, M, K, A[][], calnum[][], result, arrmin[], idx, primaryA[][];
+	static int rcsinput[][];
+	static int min = Integer.MAX_VALUE;
+	static boolean visit[];
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,21 +19,68 @@ public class bk17406 {
 		M = Integer.parseInt(st.nextToken()); // 행 입력
 		K = Integer.parseInt(st.nextToken()); // 회전 수 입력
 		A = new int[N][M];
-		int min = Integer.MAX_VALUE;
+		primaryA = new int[N][M];
+		rcsinput = new int[K][3];
+		visit = new boolean[K];
+		calnum = new int[K][3];
+		arrmin = new int[K];
+		idx = 0;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < M; j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
+				primaryA[i][j] = A[i][j];
 			}
 		}
+
 		for (int k = 0; k < K; k++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
-			rcs(r, c, s);
+			rcsinput[k][0] = r;
+			rcsinput[k][1] = c;
+			rcsinput[k][2] = s;
+
+		}
+		permutation(0);
+		
+		System.out.println(min);
+
+	}
+
+	static void permutation(int cnt) {
+
+		if (cnt == K) {
+			for (int k = 0; k < K; k++) {
+				rcs(calnum[k][0], calnum[k][1], calnum[k][2]);
+			}
+
+			rcsmin(calnum);
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					A[i][j] = primaryA[i][j];
+				}
+			}
+			return;
 		}
 
+		for (int i = 0; i < K; i++) {
+			if (visit[i])
+				continue;
+			for (int j = 0; j < 3; j++) {
+				calnum[cnt][j] = rcsinput[i][j];
+			}
+			visit[i] = true;
+			permutation(cnt + 1);
+			visit[i] = false;
+
+		}
+	}
+
+	
+	static int rcsmin(int [][]calnum) {
 		for (int i = 0; i < N; i++) {
 			int sum = 0;
 			for (int j = 0; j < M; j++) {
@@ -37,9 +88,9 @@ public class bk17406 {
 			}
 			min = Math.min(min, sum);
 		}
-		System.out.println(min);
+		return min;
 	}
-
+	
 	static void rcs(int r, int c, int s) {
 		int chk = 0;
 		int upleftY = r - s - 1;
